7cde70d063ac97a61e9c7dc49e010a241588f6d1
Fatima Muhammad <mfaliyyu@gmail.com>
2023-09-09 12:45:02 +0100

    user changed

--- 03-1-exercise.js
+++ 03-1-exercise.js
@@ -5,4 +5,5 @@ console.log("%cHello World", "color:Blue; font-size: 40px");
 * 2. Hello World, is the first argument to be printed to the browser console
 * 3. Second argument is the actual CSS styling
 *
-* Note:The effect of the styling only works on browser console.*/
+* Note:The effect of the styling only works on browser console.
+* Add random*/
